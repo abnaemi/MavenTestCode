e93720cef3b2ad68796d7e5b0a5ada2d68223822
abnaemi <ab.naemi@gmail.com>
2023-03-02 10:24:15 +0100

    2 commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+ class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+
+        // Test Code
+
+
+    }
+}
